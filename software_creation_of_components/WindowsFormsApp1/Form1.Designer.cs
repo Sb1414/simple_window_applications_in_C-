140d7b2d2af9b1bf79e4b6aad835a7fcd3eb894c
Sb1414 <kalkamanova.sabina14@gmail.com>
2022-10-09 20:00:30 +0300

    software_creation_of_components

---
+++ software_creation_of_components/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBack = new System.Windows.Forms.Panel();
+            this.labellogo = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.panelBack.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelBack
+            // 
+            this.panelBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(186)))), ((int)(((byte)(158)))));
+            this.panelBack.Controls.Add(this.labellogo);
+            this.panelBack.Controls.Add(this.buttonClose);
+            this.panelBack.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelBack.Location = new System.Drawing.Point(0, 0);
+            this.panelBack.Name = "panelBack";
+            this.panelBack.Size = new System.Drawing.Size(575, 35);
+            this.panelBack.TabIndex = 0;
+            this.panelBack.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelBack_MouseDown);
+            this.panelBack.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelBack_MouseMove);
+            // 
+            // labellogo
+            // 
+            this.labellogo.AutoSize = true;
+            this.labellogo.Font = new System.Drawing.Font("Arial Black", 12F);
+            this.labellogo.Location = new System.Drawing.Point(12, 5);
+            this.labellogo.Name = "labellogo";
+            this.labellogo.Size = new System.Drawing.Size(326, 28);
+            this.labellogo.TabIndex = 1;
+            this.labellogo.Text = "Dynamically Adding Controls";
+            this.labellogo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelBack_MouseDown);
+            this.labellogo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelBack_MouseMove);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(128)))), ((int)(((byte)(87)))));
+            this.buttonClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(237)))), ((int)(((byte)(209)))));
+            this.buttonClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(219)))), ((int)(((byte)(184)))));
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClose.Location = new System.Drawing.Point(541, 0);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(34, 33);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "x";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Arial Black", 10F);
+            this.labelName.Location = new System.Drawing.Point(17, 54);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(170, 24);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Number of inputs";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInput.Location = new System.Drawing.Point(212, 54);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(196, 26);
+            this.textBoxInput.TabIndex = 3;
+            this.textBoxInput.Enter += new System.EventHandler(this.textBoxInput_Enter);
+            this.textBoxInput.Leave += new System.EventHandler(this.textBoxInput_Leave);
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGenerate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(128)))), ((int)(((byte)(87)))));
+            this.buttonGenerate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonGenerate.FlatAppearance.BorderSize = 0;
+            this.buttonGenerate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(237)))), ((int)(((byte)(209)))));
+            this.buttonGenerate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(219)))), ((int)(((byte)(184)))));
+            this.buttonGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGenerate.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGenerate.Location = new System.Drawing.Point(434, 54);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(120, 26);
+            this.buttonGenerate.TabIndex = 2;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonGenerate.UseVisualStyleBackColor = false;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRemove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(128)))), ((int)(((byte)(87)))));
+            this.buttonRemove.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonRemove.FlatAppearance.BorderSize = 0;
+            this.buttonRemove.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(237)))), ((int)(((byte)(209)))));
+            this.buttonRemove.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(219)))), ((int)(((byte)(184)))));
+            this.buttonRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRemove.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRemove.Location = new System.Drawing.Point(434, 96);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(120, 26);
+            this.buttonRemove.TabIndex = 4;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonRemove.UseVisualStyleBackColor = false;
+            this.buttonRemove.Visible = false;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 366);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.panelBack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panelBack.ResumeLayout(false);
+            this.panelBack.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBack;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labellogo;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Button buttonRemove;
+    }
+}
+
---
+++ software_creation_of_components/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public string action;
+        public string numFirst;
+        public bool numSecond;
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxInput.Text = "enter a number";
+            textBoxInput.ForeColor = Color.Gray;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            this.AutoSize = true;
+            this.Padding = new Padding(0, 0, 0, 20);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        Point lastPoint;
+        private void panelBack_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void panelBack_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private List<TextBox> inputTextBoxes;
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            double num = 0.0;
+            string box = textBoxInput.Text;
+            if (box == "enter a number")
+            {
+                MessageBox.Show("Enter a number, please!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (double.TryParse(box, out num))
+            {
+                if (num < 1 | num > 20)
+                {
+                    MessageBox.Show("Eenter a number from 1 to 20", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                } else
+                {
+                    int inputNumber = Int32.Parse(textBoxInput.Text);
+                    inputTextBoxes = new List<TextBox>();
+                    for (int i = 1; i <= inputNumber; i++)
+                    {
+                        System.Windows.Forms.Label labelInput = new System.Windows.Forms.Label();
+                        TextBox textBoxNewInput = new TextBox();
+                        labelInput.Text = "Input " + i;
+                        labelInput.Location = new Point(30, textBoxInput.Bottom + (i * 30));
+                        labelInput.AutoSize = true;
+
+                        textBoxNewInput.Location = new Point(labelInput.Width, labelInput.Top - 3);
+                        inputTextBoxes.Add(textBoxNewInput);
+                        this.Controls.Add(labelInput);
+                        this.Controls.Add(textBoxNewInput);
+                    }
+                    Button buttonAdd = new Button();
+                    buttonAdd.Text = "Add";
+                    buttonAdd.Location = new Point(this.Width / 2 - buttonAdd.Width / 2, inputTextBoxes[inputTextBoxes.Count - 1].Bottom + 20);
+                    buttonAdd.Click += new EventHandler(buttonAdd_Click);
+                    buttonAdd.BackColor = Color.FromArgb(34, 128, 87);
+                    buttonAdd.FlatStyle = FlatStyle.Flat;
+                    buttonAdd.FlatAppearance.BorderSize = 0;
+                    this.Controls.Add(buttonAdd);
+                    this.CenterToScreen();
+
+                    buttonRemove.Visible = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Only numbers, please!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            foreach (TextBox textBoxNewInput in inputTextBoxes)
+            {
+                if (textBoxNewInput.Text == string.Empty)
+                {
+                    MessageBox.Show("Please fill in all the text boxes", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                } else
+                {
+                    double num = 0.0;
+                    string box = textBoxNewInput.Text;
+                    if (double.TryParse(box, out num))
+                    {
+                        sum += num;
+                    }
+                }
+            }
+            MessageBox.Show("The Sum is " + sum);
+        }
+
+        private void textBoxInput_Enter(object sender, EventArgs e)
+        {
+            if (textBoxInput.Text == "enter a number")
+            {
+                textBoxInput.Text = "";
+                textBoxInput.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxInput_Leave(object sender, EventArgs e)
+        {
+            if (textBoxInput.Text == "")
+            {
+                textBoxInput.Text = "enter a number";
+                textBoxInput.ForeColor = Color.Gray;
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            foreach (TextBox textBoxNewInput in inputTextBoxes)
+            {
+                this.Controls.Remove(textBoxNewInput);
+                // this.Controls.Remove(labelInput);
+            }
+        }
+    }
+}
